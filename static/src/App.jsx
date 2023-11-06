501afcf4aa18a152f519cabf0cc4c8ef920e6cd5
Anurag-a-a <112991760+Anurag-a-a@users.noreply.github.com>
2023-11-05 23:15:35 -0500

    removing unnecessary react files

--- static/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "@/route/index";
-import "./App.css";
-
-function App() {
-  return <RouterProvider router={router} />;
-}
-
-export default App;
